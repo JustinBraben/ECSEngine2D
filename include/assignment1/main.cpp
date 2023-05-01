1446877101e618abe4755738a5f097979f206c41
Justin Braben <jbraben@dmsiworks.com>
2023-05-01 10:40:28 -0600

    Finished

--- include/assignment1/main.cpp
+++ include/assignment1/main.cpp
@@ -4,6 +4,43 @@
 #include <filesystem>
 #include <SFML/Graphics.hpp>
 
+class TextComponent {
+protected:
+    std::string m_text;
+    sf::Color m_color;
+    sf::Font m_font;
+    sf::Text m_sfText;
+    float m_textSize = 30.0f;
+
+public:
+    TextComponent(const std::string& textToDisplay, const sf::Color& textColor, sf::Font& textFont, float textSize)
+        : m_text(textToDisplay), m_color(textColor), m_font(textFont), m_textSize(textSize) {
+
+        m_sfText.setFont(m_font);
+
+        m_sfText.setCharacterSize(m_textSize);
+        m_sfText.setString(m_text);
+        m_sfText.setFillColor(m_color);
+    }
+
+    void setPosition(float xPos, float yPos) {
+        m_sfText.setPosition(xPos, yPos);
+    }
+
+    void draw(sf::RenderWindow& window) const {
+        window.draw(m_sfText);
+    }
+
+    sf::Text getText() {
+        return m_sfText;
+    }
+
+    sf::Vector2f getCenter() const {
+        sf::FloatRect bounds = m_sfText.getGlobalBounds();
+        return sf::Vector2f(bounds.left + bounds.width / 2.f, bounds.top + bounds.height / 2.f);
+    }
+};
+
 class Entity {
 protected:
     float m_xPos = 40.0f;
@@ -44,106 +81,37 @@ public:
         setPosition(getX() + m_xSpeed, getY() + m_ySpeed);
     }
 
-    virtual void draw(sf::RenderWindow& window) const override = 0;
+    void draw(sf::RenderWindow& window) const override = 0;
 
     void print() const override {}
 };
 
-class Bounce : public Dynamic {
+class Shape : public Dynamic {
 protected:
-    float m_width;
-    float m_height;
-
-public:
-    Bounce(float startX, float startY, float xSpeed, float ySpeed, float width, float height)
-        : Dynamic(startX, startY, xSpeed, ySpeed), m_width(width), m_height(height) {}
-
-    void update(sf::RenderWindow& window) override {
-        float leftBound = 0.f;
-        float rightBound = window.getSize().x - m_width;
-        float topBound = 0.f;
-        float bottomBound = window.getSize().y - m_height;
-
-        // Update position
-        float newX = getX() + m_xSpeed;
-        float newY = getY() + m_ySpeed;
-
-        // Check if out of bounds
-        if (newX < leftBound || newX > rightBound) {
-            m_xSpeed *= -1.f;
-        }
-        if (newY < topBound || newY > bottomBound) {
-            m_ySpeed *= -1.f;
-        }
-
-        setPosition(newX, newY);
-    }
-
-    void draw(sf::RenderWindow& window) const override {};
-
-    void print() const override {}
-};
-
-class TextInShape {
-private:
-    std::string m_fontPath = "../../include/assignment1/configs/config.txt";
-    std::string m_shapeName = "Default shape name";
-    float m_textSize = 50;
-    sf::Color m_fontColor = sf::Color(255, 255, 255);
-    sf::Font m_font;
-public:
-    TextInShape() {}
-    TextInShape(std::string fontPath, std::string shapeName, float textSize, sf::Color fontColor, sf::Font& font)
-        : m_fontPath(fontPath), m_shapeName(shapeName), m_textSize(textSize), m_fontColor(fontColor), m_font(font) {
-    }
-
-    auto getShapeName() const { return m_shapeName; }
-    auto getTextSize() const { return m_textSize; }
-    auto getFontColor() const { return m_fontColor; }
-    auto& getFont() const { return m_font; }
-};
-
-class BouncingShape : public Bounce {
-protected:
+    std::string m_shapeType;
     sf::Color m_shapeColor;
 
 public:
-    BouncingShape(float startX, float startY, float xSpeed, float ySpeed, float width, float height, sf::Color shapeColor)
-        : Bounce(startX, startY, xSpeed, ySpeed, width, height), m_shapeColor(shapeColor)
-    {}
+    Shape(float startX, float startY, float xSpeed, float ySpeed, const std::string& shapeType, const sf::Color& shapeColor)
+        : Dynamic(startX, startY, xSpeed, ySpeed), m_shapeType(shapeType), m_shapeColor(shapeColor) {}
 
-    virtual void draw(sf::RenderWindow& window) const override = 0;
+    void draw(sf::RenderWindow& window) const override = 0;
 };
 
-class BouncingRectangle : public BouncingShape {
+class Circle : public Shape {
+protected:
+    float m_radius = 10.0f;
+    std::unique_ptr<TextComponent> m_textComponent;
+
 public:
-    BouncingRectangle(float startX, float startY, float xSpeed, float ySpeed, float width, float height, sf::Color shapeColor)
-        : BouncingShape(startX, startY, xSpeed, ySpeed, width, height, shapeColor)
-    {}
-
-    void update(sf::RenderWindow& window) override {
-        BouncingShape::update(window);
-    }
-
-    void draw(sf::RenderWindow& window) const override {
-        sf::RectangleShape rect(sf::Vector2f(m_width, m_height));
-        rect.setFillColor(m_shapeColor);
-        rect.setPosition(m_xPos, m_yPos);
-        window.draw(rect);
-    }
-};
-
-class BouncingCircle : public BouncingShape {
-public:
-    BouncingCircle(float startX, float startY, float xSpeed, float ySpeed, float width, float height, sf::Color shapeColor)
-        : BouncingShape(startX, startY, xSpeed, ySpeed, width, height, shapeColor)
-    {}
+    Circle(float startX, float startY, float xSpeed, float ySpeed, const std::string& shapeType, sf::Color& shapeColor, float radius, std::unique_ptr<TextComponent> textComponent)
+        : Shape(startX, startY, xSpeed, ySpeed, shapeType, shapeColor), m_radius(radius), m_textComponent(std::move(textComponent)) {}
 
     void update(sf::RenderWindow& window) override {
         float leftBound = 0.f;
-        float rightBound = window.getSize().x - 2 * m_width;
+        float rightBound = window.getSize().x - 2 * m_radius;
         float topBound = 0.f;
-        float bottomBound = window.getSize().y - 2 * m_width;
+        float bottomBound = window.getSize().y - 2 * m_radius;
 
         // Update position
         float newX = getX() + m_xSpeed;
@@ -175,40 +143,92 @@ public:
     }
 
     void draw(sf::RenderWindow& window) const override {
-        sf::CircleShape circ(m_width);
+        sf::CircleShape circ(m_radius);
         circ.setFillColor(m_shapeColor);
         circ.setPosition(m_xPos, m_yPos);
         window.draw(circ);
+
+        if (m_textComponent) {
+            m_textComponent->setPosition(m_xPos, m_yPos);
+
+            // Get the global bounds of the shape
+            sf::FloatRect shapeBounds = circ.getGlobalBounds();
+
+            // Get the global bounds of the text
+            sf::FloatRect textBounds = m_textComponent->getText().getGlobalBounds();
+
+            // Calculate the position of the text
+            float xText = shapeBounds.left + (shapeBounds.width - textBounds.width) / 2.0f;
+            float yText = shapeBounds.top + (shapeBounds.height - textBounds.height) / 2.0f;
+
+            // Set the position of the text
+            m_textComponent->setPosition(xText, yText);
+
+            m_textComponent->draw(window);
+        }
     }
 };
 
-class BouncingText : public Bounce {
-    std::string m_shapeName;
-    sf::Font m_font;
-    sf::Color m_textColor;
-    float m_textSize;
+class Rectangle : public Shape {
+protected:
+    float m_width = 100.0f;
+    float m_height = 100.0f;
+    std::unique_ptr<TextComponent> m_textComponent;
 
 public:
-    BouncingText(float startX, float startY, float xSpeed, float ySpeed, float width, float height, std::string shapeName, sf::Font& font, sf::Color textColor, float textSize)
-        : Bounce(startX, startY, xSpeed, ySpeed, width, height), m_shapeName(shapeName), m_font(font), m_textColor(textColor), m_textSize(textSize) {}
+    Rectangle(float startX, float startY, float xSpeed, float ySpeed, const std::string& shapeType, sf::Color& shapeColor, float width, float height, std::unique_ptr<TextComponent> textComponent)
+        : Shape(startX, startY, xSpeed, ySpeed, shapeType, shapeColor), m_width(width), m_height(height), m_textComponent(std::move(textComponent)) {}
 
-    void update(sf::RenderWindow& window) {
-        Bounce::update(window);
+    void update(sf::RenderWindow& window) override {
+        float leftBound = 0.f;
+        float rightBound = window.getSize().x - m_width;
+        float topBound = 0.f;
+        float bottomBound = window.getSize().y - m_height;
+
+        // Update position
+        float newX = getX() + m_xSpeed;
+        float newY = getY() + m_ySpeed;
+
+        // Check if out of bounds
+        if (newX < leftBound || newX > rightBound) {
+            m_xSpeed *= -1.f;
+        }
+        if (newY < topBound || newY > bottomBound) {
+            m_ySpeed *= -1.f;
+        }
+
+        setPosition(newX, newY);
     }
 
     void draw(sf::RenderWindow& window) const override {
-        sf::Text text(m_shapeName, m_font, m_textSize);
-        text.setFillColor(m_textColor);
-        sf::FloatRect textRect = text.getLocalBounds();
-        text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+        sf::RectangleShape rect(sf::Vector2f(m_width, m_height));
+        rect.setFillColor(m_shapeColor);
+        rect.setPosition(m_xPos, m_yPos);
+        window.draw(rect);
 
-        // Calculate center of circle
-        sf::Vector2f circleCenter(m_xPos + m_width / 2.0f, m_yPos + m_height / 2.0f);
+        if (m_textComponent) {
+            m_textComponent->setPosition(m_xPos, m_yPos);
 
-        // Set the text's position to the center of the circle
-        text.setPosition(circleCenter);
+            // Get the global bounds of the shape
+            sf::FloatRect shapeBounds = rect.getGlobalBounds();
 
-        window.draw(text);
+            // Get the global bounds of the text
+            sf::FloatRect textBounds = m_textComponent->getText().getGlobalBounds();
+
+            // Calculate the position of the text
+            float xText = shapeBounds.left + (shapeBounds.width - textBounds.width) / 2.0f;
+            float yText = shapeBounds.top + (shapeBounds.height - textBounds.height) / 2.0f;
+
+            // Set the position of the text
+            m_textComponent->setPosition(xText, yText);
+
+            m_textComponent->draw(window);
+        }
+    }
+
+    sf::Vector2f getCenter(sf::RectangleShape& rect) const {
+        sf::FloatRect bounds = rect.getGlobalBounds();
+        return sf::Vector2f(bounds.left + bounds.width / 2.f, bounds.top + bounds.height / 2.f);
     }
 };
 
@@ -220,15 +240,13 @@ void runAssignment1(void)
     std::ifstream fin("../../include/assignment1/configs/config.txt");
 
     float fontSize = 1;
-    sf::Font shapeFont;
+    sf::Font font;
     std::string fontPath;
     sf::Color fontColor;
 
-    TextInShape textShape;
-
     sf::RenderWindow window;
 
-    std::vector<std::unique_ptr<Bounce>> myBouncyEntities;
+    std::vector<std::unique_ptr<Shape>> myBouncyEntities;
 
     std::string line;
     while (std::getline(fin, line)) {
@@ -244,7 +262,7 @@ void runAssignment1(void)
         else if (type == "Font") {
             int r, g, b;
             ss >> fontPath >> fontSize >> r >> g >> b;
-            shapeFont.loadFromFile(fontPath);
+            font.loadFromFile(fontPath);
             fontColor = sf::Color(r, g, b);
             // Use font to set text properties
         }
@@ -253,16 +271,19 @@ void runAssignment1(void)
             float x, y, dx, dy, radius;
             int r, g, b;
             ss >> name >> x >> y >> dx >> dy >> r >> g >> b >> radius;
-            myBouncyEntities.push_back(std::make_unique<BouncingCircle>(x, y, dx, dy, radius, radius, sf::Color(r, g, b)));
-            myBouncyEntities.push_back(std::make_unique<BouncingText>(x, y, dx, dy, radius, radius, name, shapeFont, fontColor, fontSize));
+            std::cout << name << "\n";
+            auto shapeTextComponent = std::make_unique<TextComponent>(name, fontColor, font, fontSize);
+            auto circle = std::make_unique<Circle>(x, y, dx, dy, name, sf::Color(r, g, b), radius, std::move(shapeTextComponent));
+            myBouncyEntities.push_back(std::move(circle));
         }
         else if (type == "Rectangle") {
             std::string name;
             float x, y, dx, dy, width, height;
             int r, g, b;
             ss >> name >> x >> y >> dx >> dy >> r >> g >> b >> width >> height;
-            myBouncyEntities.push_back(std::make_unique<BouncingRectangle>(x, y, dx, dy, width, height, sf::Color(r, g, b)));
-            myBouncyEntities.push_back(std::make_unique<BouncingText>(x, y, dx, dy, width, height, name, shapeFont, fontColor, fontSize));
+            auto shapeTextComponent = std::make_unique<TextComponent>(name, fontColor, font, fontSize);
+            auto rect = std::make_unique<Rectangle>(x, y, dx, dy, name, sf::Color(r, g, b), width, height, std::move(shapeTextComponent));
+            myBouncyEntities.push_back(std::move(rect));
         }
     }
 
