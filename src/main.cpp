7de4c73d1ab467ecd498a4e2a278f2683f1c57ee
Justin Braben <jbraben@dmsiworks.com>
2023-05-05 17:39:26 -0600

    remove fluff

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,6 @@
 
 int main(void) 
 {
-    //runAssignment1();
-    // Enemy, all enemyEntities | Bullet, all bulletEntities
-    //Vec2 v1(100, 200);
-    //Vec2 v2(50, 75);
-    //Vec2 v3(1000, 2000);
-    //std::cout << v1.x << " " << v1.y << "\n";
-
-    std::cout << "wtf\n";
-    
     Game g("../../include/assignment2/configs/config.txt");
     g.run();
 
